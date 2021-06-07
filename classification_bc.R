70380b84ec1cbec65543df43c8991a7bc2128ca6
moticilla <ellie.pizey@gmail.com>
2021-06-07 22:27:28 +0100

    Add files via upload

---
+++ classification_bc.R
@@ -0,0 +1,166 @@
+wdbc <- read.csv("~/Documents/classification_problem/wdbc.data", header=FALSE)
+
+# this only works for consecutive rows and doesn't otherwise
+# plot(wdbc[,3:4])
+# points(wdbc[wdbc$V2=='M',3:4],col="red")
+
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V4')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V5')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V6')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V7')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V8')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V9')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V10')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V11')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V3' ,'V12')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V4' ,'V5')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V4' ,'V6')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V4' ,'V7')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V4' ,'V8')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V4' ,'V9')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V4' ,'V10')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V4' ,'V11')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V4' ,'V12')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V5' ,'V6')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V5' ,'V7')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V5' ,'V8')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V5' ,'V9')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V5' ,'V10')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V5' ,'V11')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V5' ,'V12')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V6' ,'V7')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V6' ,'V8')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V6' ,'V9')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V6' ,'V10')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V6' ,'V11')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V6' ,'V12')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V8' ,'V9')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V8' ,'V10')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V8' ,'V11')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V8' ,'V12')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V9' ,'V10')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V9' ,'V11')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V9' ,'V12')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V10' ,'V11')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V10' ,'V12')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
+
+datanew <- wdbc[, c('V2' ,'V11' ,'V12')]
+plot(datanew[,2:3])
+points(datanew[datanew$V2=='M',2:3],col="red")
